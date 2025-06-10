93654f12f82442f16d5eb20a5d8af8ff01088c73
NikKowPHP <nik.kow@outlook.com>
2025-06-10 14:20:43 +0200

    feat: Update ProgressIndicator component and add Storybook stories

--- project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
+++ project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
@@ -17,55 +17,71 @@ Default.args = {
 export const WithLabel = Template.bind({});
 WithLabel.args = {
   value: 75,
-  label: 'Uploading...',
-};
-
-export const WithMax = Template.bind({});
-WithMax.args = {
-  value: 42,
-  max: 100,
-};
-
-export const WithMin = Template.bind({});
-WithMin.args = {
-  value: 60,
-  min: 30,
-  max: 90,
-};
-
-export const WithStep = Template.bind({});
-WithStep.args = {
-  value: 20,
-  step: 10,
-  max: 100,
-};
-
-export const WithBuffer = Template.bind({});
-WithBuffer.args = {
-  value: 50,
-  buffer: 75,
-};
-
-export const WithVariant = Template.bind({});
-WithVariant.args = {
-  value: 60,
-  variant: 'success',
+  label: 'Upload Progress',
 };
 
 export const WithSize = Template.bind({});
 WithSize.args = {
-  value: 40,
+  value: 30,
   size: 'large',
 };
 
-export const WithStriped = Template.bind({});
-WithStriped.args = {
-  value: 30,
-  striped: true,
+export const WithColor = Template.bind({});
+WithColor.args = {
+  value: 60,
+  color: 'blue',
 };
 
-export const WithAnimated = Template.bind({});
-WithAnimated.args = {
+export const WithBackgroundColor = Template.bind({});
+WithBackgroundColor.args = {
   value: 45,
+  backgroundColor: 'lightgray',
+};
+
+export const WithStrokeWidth = Template.bind({});
+WithStrokeWidth.args = {
+  value: 80,
+  strokeWidth: 5,
+};
+
+export const WithAnimation = Template.bind({});
+WithAnimation.args = {
+  value: 50,
   animated: true,
+};
+
+export const WithDeterminate = Template.bind({});
+WithDeterminate.args = {
+  value: 25,
+  determinate: true,
+};
+
+export const WithIndeterminate = Template.bind({});
+WithIndeterminate.args = {
+  indeterminate: true,
+};
+
+export const WithText = Template.bind({});
+WithText.args = {
+  value: 50,
+  showText: true,
+};
+
+export const WithCustomStyle = Template.bind({});
+WithCustomStyle.args = {
+  value: 70,
+  style: { marginTop: '20px' },
+};
+
+export const WithOnComplete = Template.bind({});
+WithOnComplete.args = {
+  value: 0,
+  onComplete: () => alert('Progress completed!'),
+};
+
+export const WithStep = Template.bind({});
+WithStep.args = {
+  value: 3,
+  max: 5,
+  showText: true,
 };
