16792da8e36055ef8cafd83678fe46bf03a536e0
NikKowPHP <nik.kow@outlook.com>
2025-06-10 14:10:43 +0200

    feat: Update ProgressIndicator component and add Storybook stories

--- project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
+++ project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
@@ -9,46 +9,65 @@ export default {
 
 const Template: StoryFn = (args) => <ProgressIndicator {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
   value: 50,
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  value: 30,
-  size: 'small',
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  value: 75,
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  value: 70,
   size: 'large',
+  value: 25,
 };
 
 export const WithLabel = Template.bind({});
 WithLabel.args = {
-  value: 45,
-  label: 'Uploading...',
+  size: 'medium',
+  value: 60,
+  label: 'In Progress',
 };
 
 export const Determinate = Template.bind({});
 Determinate.args = {
-  value: 60,
+  size: 'large',
+  value: 85,
   determinate: true,
 };
 
 export const Indeterminate = Template.bind({});
 Indeterminate.args = {
+  size: 'medium',
   indeterminate: true,
 };
 
 export const WithCustomStyles = Template.bind({});
 WithCustomStyles.args = {
-  value: 50,
+  size: 'large',
+  value: 40,
   styles: {
     backgroundColor: '#e0e0e0',
-    trackColor: '#b0b0b0',
-    thumbColor: '#4caf50',
+    barColor: '#4caf50',
+    borderRadius: '5px',
   },
+};
+
+export const WithOnComplete = Template.bind({});
+WithOnComplete.args = {
+  size: 'medium',
+  value: 100,
+  onComplete: () => alert('Progress completed!'),
+};
+
+export const WithTooltip = Template.bind({});
+WithTooltip.args = {
+  size: 'large',
+  value: 30,
+  showTooltip: true,
 };
