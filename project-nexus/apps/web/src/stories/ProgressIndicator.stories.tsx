668e74919a962f7fa1565135df788cc37ee0ddc4
NikKowPHP <nik.kow@outlook.com>
2025-06-10 14:26:55 +0200

    feat: Update ProgressIndicator component and add Storybook stories

--- project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
+++ project-nexus/apps/web/src/stories/ProgressIndicator.stories.tsx
@@ -14,74 +14,158 @@ Default.args = {
   value: 50,
 };
 
-export const WithLabel = Template.bind({});
-WithLabel.args = {
-  value: 75,
-  label: 'Upload Progress',
+export const WithMinMax = Template.bind({});
+WithMinMax.args = {
+  value: 50,
+  min: 0,
+  max: 100,
 };
 
-export const WithSize = Template.bind({});
-WithSize.args = {
-  value: 30,
-  size: 'large',
+export const WithStep = Template.bind({});
+WithStep.args = {
+  value: 50,
+  step: 10,
+};
+
+export const WithBuffer = Template.bind({});
+WithBuffer.args = {
+  value: 50,
+  buffer: 75,
 };
 
 export const WithColor = Template.bind({});
 WithColor.args = {
-  value: 60,
+  value: 50,
   color: 'blue',
 };
 
 export const WithBackgroundColor = Template.bind({});
 WithBackgroundColor.args = {
-  value: 45,
+  value: 50,
   backgroundColor: 'lightgray',
 };
 
-export const WithStrokeWidth = Template.bind({});
-WithStrokeWidth.args = {
-  value: 80,
-  strokeWidth: 5,
-};
-
-export const WithAnimation = Template.bind({});
-WithAnimation.args = {
+export const WithSize = Template.bind({});
+WithSize.args = {
   value: 50,
-  animated: true,
+  size: 'large',
 };
 
-export const WithDeterminate = Template.bind({});
-WithDeterminate.args = {
-  value: 25,
-  determinate: true,
-};
-
-export const WithIndeterminate = Template.bind({});
-WithIndeterminate.args = {
-  indeterminate: true,
-};
-
-export const WithText = Template.bind({});
-WithText.args = {
+export const WithRounded = Template.bind({});
+WithRounded.args = {
   value: 50,
-  showText: true,
+  rounded: 'full',
+};
+
+export const WithBorder = Template.bind({});
+WithBorder.args = {
+  value: 50,
+  border: '2px solid #000',
+};
+
+export const WithShadow = Template.bind({});
+WithShadow.args = {
+  value: 50,
+  shadow: 'md',
+};
+
+export const WithOnChange = Template.bind({});
+WithOnChange.args = {
+  value: 50,
+  onChange: (value: number) => alert(`Value changed: ${value}`),
+};
+
+export const WithOnClick = Template.bind({});
+WithOnClick.args = {
+  value: 50,
+  onClick: () => alert('ProgressIndicator clicked'),
+};
+
+export const WithTooltip = Template.bind({});
+WithTooltip.args = {
+  value: 50,
+  tooltip: 'Progress: 50%',
 };
 
 export const WithCustomStyle = Template.bind({});
 WithCustomStyle.args = {
-  value: 70,
-  style: { marginTop: '20px' },
+  value: 50,
+  style: { borderRadius: '50px' },
 };
 
-export const WithOnComplete = Template.bind({});
-WithOnComplete.args = {
-  value: 0,
-  onComplete: () => alert('Progress completed!'),
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  value: 50,
+  label: 'Progress:',
 };
 
-export const WithStep = Template.bind({});
-WithStep.args = {
-  value: 3,
-  max: 5,
-  showText: true,
+export const WithLabelPosition = Template.bind({});
+WithLabelPosition.args = {
+  value: 50,
+  label: 'Progress:',
+  labelPosition: 'top',
+};
+
+export const WithLabelColor = Template.bind({});
+WithLabelColor.args = {
+  value: 50,
+  label: 'Progress:',
+  labelColor: 'blue',
+};
+
+export const WithLabelSize = Template.bind({});
+WithLabelSize.args = {
+  value: 50,
+  label: 'Progress:',
+  labelSize: 'large',
+};
+
+export const WithLabelStyle = Template.bind({});
+WithLabelStyle.args = {
+  value: 50,
+  label: 'Progress:',
+  labelStyle: { fontWeight: 'bold' },
+};
+
+export const WithLabelClassName = Template.bind({});
+WithLabelClassName.args = {
+  value: 50,
+  label: 'Progress:',
+  labelClassName: 'custom-label',
+};
+
+export const WithDisabled = Template.bind({});
+WithDisabled.args = {
+  value: 50,
+  disabled: true,
+};
+
+export const WithLoading = Template.bind({});
+WithLoading.args = {
+  value: 50,
+  loading: true,
+};
+
+export const WithIndeterminate = Template.bind({});
+WithIndeterminate.args = {
+  value: 50,
+  indeterminate: true,
+};
+
+export const WithStriped = Template.bind({});
+WithStriped.args = {
+  value: 50,
+  striped: true,
+};
+
+export const WithAnimated = Template.bind({});
+WithAnimated.args = {
+  value: 50,
+  animated: true,
+};
+
+export const WithResponsive = Template.bind({});
+WithResponsive.args = {
+  value: 50,
+  responsive: true,
 };
